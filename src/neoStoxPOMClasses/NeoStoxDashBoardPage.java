9a56cd3e14468a85858aab099de48759f6afbef8
Tushar <tushar.durge2013@gmail.com>
2023-02-04 22:03:31 +0530

    pom dash line7

--- src/neoStoxPOMClasses/NeoStoxDashBoardPage.java
+++ src/neoStoxPOMClasses/NeoStoxDashBoardPage.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-//hii all.
+//hii all
 public class NeoStoxDashBoardPage
 {
    @FindBy(xpath = "(//a[text()='OK'])[2]") private WebElement popUpokButton;
